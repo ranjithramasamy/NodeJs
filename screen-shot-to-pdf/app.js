eb4d1128f10bb21a97b6eea79ed29139a26f0940
Ranjith Ramasamy <ranjith.ramasamy@risksense.com>
2019-08-09 19:04:10 +0530

    Demonstration of screenshot and PDF file generation by using Puppeteer library

--- screen-shot-to-pdf/app.js
+++ screen-shot-to-pdf/app.js
@@ -1,8 +1,11 @@
 const commonUtil = require('./utils/common-util');
 const logger = require('./utils/common-logger');
 
-logger.info("App", "Please wait, given webpage is being converted to image.");
-commonUtil.screenshot("https://nodejs.org/en/about/", "./output/screen-shot.png");
+const webPageUrl = "https://nodejs.org/en/about/";
+const moduleName = "App";
 
-logger.info("App", "Please wait, given webpage is being converted to PDF.");
-commonUtil.pdf("https://nodejs.org/en/about/", "./output/screen-shot.pdf");
+logger.info(moduleName, "Please wait, given webpage is being converted to image.");
+commonUtil.screenshot(webPageUrl, "./output/screen-shot.png");
+
+logger.info(moduleName, "Please wait, given webpage is being converted to PDF.");
+commonUtil.pdf(webPageUrl, "./output/screen-shot.pdf");
